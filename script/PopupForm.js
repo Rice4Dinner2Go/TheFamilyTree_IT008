f49dd607d40b2c5b6ba10217fb151ff15aa1e131
Rice4Dinnr <truongkienthanh6705@gmail.com>
2024-11-24 18:26:37 +0700

    Revert "Merge branch '(Test)-Main-merge-Dev' into dev"
    This reverts commit 4e031847cf1db4afb37924028a6fb69473f9885d, reversing
    changes made to 1e5b15932ccb2ddb8e17a07b964af9218bd01864.

--- script/PopupForm.js
+++ script/PopupForm.js
@@ -1,25 +1,7 @@
-function showNewTreeForm() {
+function showForm() {
     document.getElementById('popupForm-newTree').style.display = 'flex';
 }
 
-function hideNewTreeForm() {
+function hideForm() {
     document.getElementById('popupForm-newTree').style.display = 'none';
-}
-
-function showImportForm() {
-    document.getElementById('popupForm-import').style.display = 'flex';
-}
-
-function hideImportForm() {
-    document.getElementById('popupForm-import').style.display = 'none';
-}
-
-function importFromJson() {
-    // TODO: Tạo chức năng import JSON
-    console.log('Import from JSON clicked');
-}
-
-function importFromExcel() {
-    // TODO: Tạo chức năng import Excel
-    console.log('Import from Excel clicked');
 }
